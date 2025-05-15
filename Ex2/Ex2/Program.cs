0a17dbe5555778a44fef7839bb4b5567993e5e78
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-05-14 22:04:32 -0300

    Add files via upload

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1;
+            double valor2;
+
+            Console.WriteLine("Digite o primeiro valor");
+            valor1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor diferente do primeiro");
+            valor2 = double.Parse(Console.ReadLine());
+
+            if (valor1 > valor2)
+            {
+
+                Console.WriteLine("O maior número digitado é: {0}", valor1);
+
+            }
+            else if (valor1 == valor2)
+            {
+
+                Console.WriteLine("Os valores são iguais");
+
+
+            }
+            else
+            {
+                Console.WriteLine("O maior número digitado é: {0}", valor2);
+            }
+        }
+    }
+}
