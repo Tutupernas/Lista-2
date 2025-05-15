f214906e22b6d15cbfc9acd46c4d3e3ebe9e3a3a
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-05-14 22:03:28 -0300

    Add files via upload

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1;
+            double valor2;
+
+            Console.WriteLine("Digite o primeiro valor");
+            valor1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor diferente do primeiro");
+            valor2 = double.Parse(Console.ReadLine());
+
+            if (valor1 > valor2)
+            {
+                Console.WriteLine("O maior número digitado é: {0}", valor1);
+            }
+            else
+            {
+                Console.WriteLine("O maior número digitado é: {0}", valor2);
+            } 
+        }
+    }
+}
