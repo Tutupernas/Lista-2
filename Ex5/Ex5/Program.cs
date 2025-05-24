71714c867a8e831f4170f4b893f3061703fc8e59
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-05-23 22:09:01 -0300

    Add files via upload

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double basee;
+            double altura;
+            double area;
+
+            Console.WriteLine("Digite um valor para a base do retângulo:");
+            basee = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite agora o valor da altura desse retângulo:");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            area = basee * altura;
+
+            if (area > 100)
+            {
+
+                Console.WriteLine("\r\nA área do terreno é de: {0} \r\n \r\nTerreno Grande!", area);
+
+            }
+            else if (area > 0 && area <= 100)
+            {
+
+                Console.WriteLine("A área do terreno é de: {0} \r\n \r\nTerreno Pequeno!", area);
+
+            }
+            else
+            {
+                Console.WriteLine("\r\nNúmero Inválido.");
+
+            }
+        }
+    }
+}
