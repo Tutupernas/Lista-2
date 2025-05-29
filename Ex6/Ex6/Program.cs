3251ad65b6ace61151480ec0da2b31ce3577743f
Tutupernas <98492436+Tutupernas@users.noreply.github.com>
2025-05-28 23:20:05 -0300

    Add files via upload

---
+++ Ex6/Ex6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+            double altura;
+
+
+            Console.WriteLine("Digite seu peso");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            if (imc < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (imc <= 20 && imc < 25)
+            {
+
+                Console.WriteLine("Peso ideal");
+            }
+            else
+            {
+
+                Console.WriteLine("Acima do peso");
+            }
+
+            Console.WriteLine(imc);
+        }
+    }
+}
